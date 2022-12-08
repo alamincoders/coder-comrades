50015b68462917a150d686b9a2b5fa676b083e3c
alamincoders <developeralamin03@gmail.com>
2022-12-08 11:32:12 +0600

    updated

--- client/app/head.tsx
+++ client/app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Coder Comrades - Online IT Platform</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
-  )
+  );
 }
